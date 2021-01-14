e86c02a979099319a87e448e00005cd4f749455a
Shawn Potter <spotter@mail.greenriver.edu>
2021-01-14 15:32:54 -0500

    updated index.php and created the views folder with home.html

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
 <?php
-//turn on error reporting
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+  //The Controller
 
-//require autoload file
-require_once("vendor/autoload.php");
+  //turn on error reporting
+  ini_set('display_errors', 1);
+  error_reporting(E_ALL);
 
-//create an instance of the base class
-$f3 = Base::instance();
-$f3->set('DEBUG', 3);
+  //require autoload file
+  require_once("vendor/autoload.php");
 
-//run fat free
-$f3->run();
+  //create an instance of the base class
+  $f3 = Base::instance();
+  $f3->set('DEBUG', 3);
+
+
+
+  //Define a default route (home page)
+  $f3->route('GET /', function(){
+    $view = new Template();
+    echo $view->render('views/home.html');
+  });
+
+
+
+  //run fat free
+  $f3->run();
